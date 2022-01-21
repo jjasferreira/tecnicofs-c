ddd0e5ec7a29c91a3801f627a46f36cbcd8fcc56
José João Ferreira <jjasferreira@gmail.com>
2022-01-21 17:34:28 +0000

    Starting the implementation of the 2nd exercise

--- p2/client/tecnicofs_client_api.c
+++ p2/client/tecnicofs_client_api.c
@@ -1,8 +1,19 @@
 #include "tecnicofs_client_api.h"
+#include <fcntl.h>
 
 int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
-    /* TODO: Implement this */
-    return -1;
+    // TODO
+    int fcli;
+    unlink(client_pipe_path);
+
+    if (mkfifo(client_pipe_path, 0777) < 0)
+        exit(1);
+    if ((fcli = open(client_pipe_path, O_WRONLY)) < 0)
+        exit(1);
+    
+    lose (fcli);
+    unlink(client_pipe_path);
+    return 0;
 }
 
 int tfs_unmount() {
@@ -22,6 +33,7 @@ int tfs_close(int fhandle) {
 
 ssize_t tfs_write(int fhandle, void const *buffer, size_t len) {
     /* TODO: Implement this */
+    // write (fserv, buf, TAMMSG);
     return -1;
 }
 
--- p2/fs/tfs_server.c
+++ p2/fs/tfs_server.c
@@ -1,6 +1,9 @@
 #include "operations.h"
+#include <fcntl.h>
 
 int main(int argc, char **argv) {
+
+    char* session[MAX_SESSIONS];
 
     if (argc < 2) {
         printf("Please specify the pathname of the server's pipe.\n");
@@ -10,7 +13,23 @@ int main(int argc, char **argv) {
     char *pipename = argv[1];
     printf("Starting TecnicoFS server with pipe called %s\n", pipename);
 
-    /* TO DO */
+    int fserv;
+    unlink(pipename);
 
+    if (mkfifo(pipename, 0777) < 0)
+        exit(1);
+    if ((fserv = open(pipename, O_RDONLY)) < 0)
+        exit(1);
+    
+    // TODO
+    /*for (;;) {
+        n = read (fserv, buf, TAMMSG);
+        if (n <= 0) break;
+        trataPedido (buf);
+        n = write (fcli, buf, TAMMSG);
+    }
+    */
+    close (fserv);
+    unlink(pipename);
     return 0;
 }
